35d652a368b56aa770b414c6f46dc0dbef1da28b
Vaibhav Raju Konnur <VA20587219@wipro.com>
2026-02-02 16:12:37 +0530

    add . UC3 added Generic Quantity class for DRY Principle

---
+++ Length.java
@@ -0,0 +1,63 @@
+public class Length{
+
+    double value;
+    private LengthUnit unit;
+
+    public enum LengthUnit{
+        FEET(12.0),
+        Inches(1.0),
+        YARD(36.0),
+        CM(1.0/2.54);
+        
+        private final double conversionFactor;
+
+        LengthUnit(double conversionFactor){
+            this.conversionFactor = conversionFactor;
+        }
+        public double getConversionFactor(){
+            return conversionFactor;
+        }
+        
+
+    }
+    public Length(double value, LengthUnit unit){
+            this.value = value;
+            this.unit = unit;
+        }
+
+    // convert to inches
+    public double convertToBaseUnit(){
+        return this.value*this.unit.getConversionFactor();
+    }
+
+    @Override
+    public  boolean equals(Object obj){
+        boolean boolResult = false;
+      
+
+        if(this == null){
+            return false;
+        }
+        if(this == obj){
+            return true;
+        }
+        // typecasting
+        Length val2 = (Length) obj;
+
+        double value1 = this.convertToBaseUnit();
+        double value2 = val2.convertToBaseUnit();
+
+        int result = Double.compare(value1,value2);
+        if(result == 0){
+            boolResult = true;
+        }
+        return boolResult;
+    }
+
+public static void main(String[] args){
+    Length len1 = new Length(1.0, LengthUnit.YARD);
+    Length len2 = new Length(360.0, LengthUnit.Inches);
+    System.out.println(len1.equals(len2));
+}
+}
+
